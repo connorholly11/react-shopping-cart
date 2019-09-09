9c8a670340e0782a0021e660dffc06ca7ec7be53
Connor Holly <connorholly11@gmail.com>
2019-09-09 14:21:11 -0400

    done with putting provider in App

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import {ProductContext} from './contexts/ProductContext';
 
 function App() {
 	const [products] = useState(data);
@@ -16,6 +17,7 @@ function App() {
 	};
 
 	return (
+		<ProductContext.Provider value={{products, addItem}}>
 		<div className="App">
 			<Navigation cart={cart} />
 
@@ -23,19 +25,15 @@ function App() {
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				component={Products}
 			/>
 
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				component={ShoppingCart}
 			/>
 		</div>
+		</ProductContext.Provider>
 	);
 }
 
